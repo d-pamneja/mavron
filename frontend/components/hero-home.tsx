ea88e504e7efaf189688d28f1622965fb690cca6
d-pamneja <dpamneja@gmail.com>
2024-10-21 11:48:27 +0530

    Initial

---
+++ frontend/components/hero-home.tsx
@@ -0,0 +1,113 @@
+// import VideoThumb from "@/public/images/hero-image-01.jpg";
+// import ModalVideo from "@/components/modal-video";
+// import Image from "next/image";
+
+export default function HeroHome() {
+  return (
+    <section>
+      <div className="mx-auto max-w-6xl px-4 sm:px-6">
+        {/* Hero content */}
+        <div className="py-12 md:py-20">
+          {/* Section header */}
+          <div className="pb-12 text-center md:pb-20">
+            <h1
+              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
+              data-aos="fade-up"
+            >
+              Ignite Your Vision with Prometheus
+            </h1>
+            <div className="mx-auto max-w-3xl">
+              <p
+                className="mb-8 text-xl text-indigo-200/65"
+                data-aos="fade-up"
+                data-aos-delay={200}
+              >
+                Unlock the full potential of your business with our expert web development services—let’s get started today!
+              </p>
+              <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
+                <div data-aos="fade-up" data-aos-delay={400}>
+                  <a
+                    className="btn group mb-4 w-full bg-gradient-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_theme(colors.white/.16)] hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
+                    href="#0"
+                  >
+                    <span className="relative inline-flex items-center">
+                      See our Projects
+                      <span className="ml-1 tracking-normal text-white/50 transition-transform group-hover:translate-x-0.5">
+                        -&gt;
+                      </span>
+                    </span>
+                  </a>
+                </div>
+                <div data-aos="fade-up" data-aos-delay={600}>
+                  <a
+                    className="btn relative w-full bg-gradient-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%] sm:ml-4 sm:w-auto"
+                    href="#0"
+                  >
+                    Schedule a Meet
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Code Animation*/}
+          <div
+            className="mx-auto max-w-3xl"
+            data-aos="zoom-y-out"
+            data-aos-delay={600}
+          >
+            <div className="relative aspect-video rounded-2xl bg-gray-900 px-5 py-3 shadow-xl before:pointer-events-none before:absolute before:-inset-5 before:border-y before:[border-image:linear-gradient(to_right,transparent,theme(colors.slate.300/.8),transparent)1] after:absolute after:-inset-5 after:-z-10 after:border-x after:[border-image:linear-gradient(to_bottom,transparent,theme(colors.slate.300/.8),transparent)1]">
+              <div className="relative mb-8 flex items-center justify-between before:block before:h-[9px] before:w-[41px] before:bg-[length:16px_9px] before:[background-image:radial-gradient(circle_at_4.5px_4.5px,_theme(colors.gray.600)_4.5px,_transparent_0)] after:w-[41px]">
+                <span className="text-[13px] font-medium text-white">
+                  prometheus.in
+                </span>
+              </div>
+              <div className="font-mono text-white-500 [&_span]:opacity-0">
+                <span className="animate-[code-1_10s_infinite] text-green-200">
+                  npm install prometheus
+                </span>{" "}
+                <span className="animate-[code-2_10s_infinite]">
+                  -- Installing everything you need to power your digital solutions...
+                </span>
+                <br />
+                <br />
+                <span className="animate-[code-3_10s_infinite] text-green-200">
+                  npm run dev
+                </span>{" "}
+                <span className="animate-[code-4_10s_infinite]">
+                  -- Spinning up your project, where ideas transform into real-world solutions...
+                </span>
+                <br />
+                <br />
+                <span className="animate-[code-5_10s_infinite] text-green-200">
+                  npm publish
+                </span>
+                <br />
+                <span className="animate-[code-6_10s_infinite]">
+                  Application deployed, your vision is now live.
+                </span>
+                <br/>
+                <br/>
+                <span className="animate-[code-7_10s_infinite] text-green-300">Welcome to Prometheus Freelance Solutions</span>{" "}
+                <br />
+                <span className="animate-[code-7_10s_infinite] text-green-300">Empowering your business with tailored end to end web development services.</span>{" "}
+                <br />
+                <span className="cursor-blink animate-blink">_</span>
+              </div>
+            </div>
+          </div>
+
+          {/* <ModalVideo
+            thumb={VideoThumb}
+            thumbWidth={1104}
+            thumbHeight={576}
+            thumbAlt="Modal video thumbnail"
+            video="videos//video.mp4"
+            videoWidth={1920}
+            videoHeight={1080}
+          /> */}
+        </div>
+      </div>
+    </section>
+  );
+}
